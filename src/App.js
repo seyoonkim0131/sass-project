750f2406903e4a97e9b648030b08ecd86e5e1b4c
System Administrator <root@qmit-mac1ui-MacBook-Pro.local>
2018-08-20 19:55:29 +0900

    2nd Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import styled, { injectGlobal, keyframes } from "styled-components";
+import styled, { injectGlobal, ThemeProvider } from "styled-components";
+import theme from "./theme.js";
+
 
 injectGlobal`
     body {
@@ -8,57 +10,34 @@ injectGlobal`
     }
 `;
 
-class App extends Component {
-    render() {
-        return (
-            <Container>
-                <Button>Hello</Button>
-                <Button danger rotationTime={5}>Hello</Button>
-                <Anchor href="http://google.com">Go to google</Anchor>
-            </Container>
-
-        );
-    }
-}
-
 const Container = styled.div`
     height: 100vh;
     width: 100%;
     background-color: pink;
 `;
 
+const Card = styled.div`
+    background-color: red;
+`;
+
 const Button = styled.button`
-    border-radius: 50px;
-    padding: 5px;
-    min-width: 120px;
-    color: #FFFFFF;
-    font-weight: 600;
-    -webkit-appearance: none;
-    cursor: pointer;
-    &:active, &:focus {
-        outline: none;
-    }
-    background-color: ${props => (props.danger ?"#e74c3c":"#2ecc71")};
-    ${props => {
-        if(props.danger) {
-            return `animation: ${rotation} ${props.rotationTime}s linear infinite`;
-        }
-}}
+    border-radius: 30px;
+    padding: 25px 15px;
+    background-color: ${props => props.theme.successColor}
 `;
 
-const Anchor = Button.withComponent("a").extend`
-    text-decoration: none;
-`; //a tag 도 button 과 같은 스타일 적용
-
-
-const rotation = keyframes`
-    from {
-        transform: rotate(0deg);
+class App extends Component {
+    render() {
+        return (
+            <ThemeProvider theme={theme}>
+                <Container>
+                    <Form />
+                </Container>
+            </ThemeProvider>
+        );
     }
-    to {
-        transform: rotate(360deg);
-    }
-`;
+}
 
+const Form = () => (<Card><Button>Hello</Button></Card>);
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,8 @@
+const theme = {
+    mainColor: "#3498db",
+    dangerColor: "#e74c3c",
+    successColor: "#2ecc71",
+
+};
+
+export default theme;
